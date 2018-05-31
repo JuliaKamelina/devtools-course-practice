4cf9bbc64c30e2a74eaccfcb3f18ddb31eededae
jkamelin <julia.kamelina@intel.com>
2018-05-31 12:11:28 +0300

    space

--- modules/binary-template-heap/include/prim-app.hpp
+++ modules/binary-template-heap/include/prim-app.hpp
@@ -10,18 +10,18 @@ using std::vector;
 
 class PrimApp {
  public:
-   PrimApp();
-   std::string operator()(int argc, const char** argv);
+    PrimApp();
+    std::string operator()(int argc, const char** argv);
 
  private:
-   void help(const char* message = "");
-   bool validateArguments(int argc, const char** argv);
-   std::string message_;
+    void help(const char* message = "");
+    bool validateArguments(int argc, const char** argv);
+    std::string message_;
 
-   typedef struct {
-     vector<vector<int> > graph;
-     int dem;
-   } Arguments;
+  typedef struct {
+    vector<vector<int> > graph;
+    int dem;
+  } Arguments;
 };
 
 #endif  //  MODULES_TEMPLATE_HEAP_INCLUDE_PRIM_APP_HPP_
